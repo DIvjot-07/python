5074c43bd1a1949d018fcb123d61ae73238139fe
Divjot bedi <divjotbedi16706@gmail.com>
2026-07-14 22:06:16 +0530

    Add files via upload

---
+++ merge_sort.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        for i in range(n):
+            nums1[i+m]=nums2[i]
+        nums1[:]=sorted(nums1)
+        return nums1
+print(Solution().merge(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3))
