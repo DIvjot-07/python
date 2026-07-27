ae6199ead9ae7fb1fda0229a917c29b4a64ae465
Divjot bedi <divjotbedi16706@gmail.com>
2026-07-27 23:17:57 +0530

    Add files via upload

---
+++ valid_parenthesis.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack=[]
+        for i in s:
+            if i == "[" or i == "{" or i == "(":
+                stack.append(i)
+            else:
+                if stack != [] and ((i == "]" and stack[-1] == "[") or (i == "}" and stack[-1] == "{") or (i == ")" and stack[-1] == "(")):
+                    stack.pop()
+                else:
+                    return False
+        if stack == []:
+            return True
+        else:
+            return False
+            
