76334f37dda9bcc93c0b7025543ce051396bfea3
Divjot bedi <divjotbedi16706@gmail.com>
2026-08-01 19:02:31 +0530

    Add files via upload

---
+++ Shuffle_the_array.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def shuffle(self, nums, n):
+        res=[]
+        for i in range(n):
+            res.append(nums[i])
+            res.append(nums[i+n])
+        return res
+    
+print(Solution().shuffle([2,5,1,3,4,7], n = 3))
