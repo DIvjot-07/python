7cc7f3a7c7b0ea0350a31319b2b64cdffc98a8cc
Divjot Bedi <divjotbedi16706@gmail.com>
2026-08-24 23:31:07 +0530

    Add files via upload

---
+++ stonegame8.py
@@ -0,0 +1,15 @@
+import itertools
+from typing import List
+
+
+class Solution:
+    def stoneGameVIII(self, stones: List[int]) -> int:
+        n = len(stones)
+        prefix = list(itertools.accumulate(stones))
+
+        dp = prefix[n - 1]
+
+        for i in range(n - 2, 0, -1):
+            dp = max(dp, prefix[i] - dp)
+
+        return dp
