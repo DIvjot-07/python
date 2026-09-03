d175932ecb35bae5a0750e27e595c7c3a0cb951d
Divjot Bedi <divjotbedi16706@gmail.com>
2026-09-03 23:18:43 +0530

    Implement uniformArray method in Solution class

--- construct_uniform_pariity_array_2.py
+++ construct_uniform_pariity_array_2.py
@@ -1,1 +1,22 @@
-
+class Solution(object):
+    def uniformArray(self, nums1):
+        """
+        :type nums1: List[int]
+        :rtype: bool
+        """
+        odd, even=0,0
+        m=float("inf")
+        n=float("inf")
+        for i in nums1:
+            if n>i:
+                n=i
+            if i%2 == 0:
+                even += 1
+            else:
+                if i<m:
+                    m=i
+                odd += 1
+            
+        if odd == 0 or m == n:
+            return True 
+        return False
